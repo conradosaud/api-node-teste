9daf7457d1bfa97a3c7b5a2533f1239e9a1e83bb
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2022-01-20 02:40:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const routes = require('./routes/routes');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
