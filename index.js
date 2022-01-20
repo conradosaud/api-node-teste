ee6f3578cb2bebaeb8e0e2264b6600b3b20252ce
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2022-01-20 04:11:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const sequelize = require('./database/database');
 const routes = require('./routes/routes');
 
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
